b9a8bfd8da6d0eb355fed8923e3aba803e3b8fc4
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2024-03-30 12:12:46 -0300

    Add files via upload

--- go-web.bat
+++ go-web.bat
@@ -1,2 +1,4 @@
-runtime\python.exe infer-web.py --pycmd runtime\python.exe --port 7897
-pause
+start /B "" runtime\python.exe infer-web.py --pycmd runtime\python.exe --port 7897
+timeout /t 5 >nul 2>&1
+start /B "" cmd /c "cd file_uploader && npm run start --host localhost --port 3000"
+start "" "http://localhost:3000"
